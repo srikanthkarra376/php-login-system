39463bc150e4d32c912bd27bb785ddc677f0f646
srikanth Karra <srikanthreddy376@gmail.com>
2019-03-27 16:05:46 +0100

    added login errors and full functionality

--- login.php
+++ login.php
@@ -1,52 +1,59 @@
 <?php 
+//start the session in all pages 
 session_start();
+//check if the form is submited 
 if(isset($_POST["login-btn"])){
-
+  //include the db credentials 
   include 'includes/db.config.php';
   //get the form values 
   $login_uname = $_POST["login-uname"];
   $login_pwd = $_POST["login-pwd"];
   //create an array to store errors !
   $login_errors = array();
-
+  //check empty username
   if(empty($login_uname)) {
     $login_errors["login_uname_err"] = "Invalid User name !";
-  
   }
-
+   //check the empty passord
   if(empty($login_pwd)) {
     $login_errors["login_pwd_err"] = "Invalid password !";
-    
-  }
 
+ }
+ //check for the erreors in the array 
   if(count($login_errors)==0) {
     
     //query database to check for existing users 
   $stmt = "SELECT * FROM users WHERE username='$login_uname'";
-
+   //prepare the mysql query 
   if($result = mysqli_query($connection, $stmt)) {
-
-    if(mysqli_num_rows($result) == 1){
+    //condition to check for the returned results is true or not 
+    if(mysqli_num_rows($result)==1){
+     // convert the result variables in to array loop through the array fetch the values from the array
       while($row = mysqli_fetch_array($result)){ 
-        $_SESSION["myname"] = $row["username"];
-
-        $passwordVerify= password_verify($row["password"]);
-        if($login_pwd == $passwordVerify) {
-         
+      
+        //comapre the passowrd of user entered and db password against hasshed password
+        $passwordVerify= password_verify($login_pwd,$row["password"]);
+       
+        if($passwordVerify) {
+            //if password verifies then set the username in to session
+          $_SESSION["myname"] = $row["username"];
+          // redirect the user to   the index page 
           header("Location:index.php");
-        }
 
-      }
-      header("Location:index.php");
+        } else {
+          $db_check_password_err = "wrong password!";
+        }
       }
     
+      } else {
+        $db_check_username_err = "Username does not exit !";
+      }
+  
     }
 
   }
 
 }
-
-
 ?>
 <div class="container col-md-6">
 
@@ -56,13 +63,26 @@ if(isset($_POST["login-btn"])){
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
   <div class="form-group">
     <input type="text" class="form-control" name="login-uname" placeholder="Username" value="<?php if(isset($login_uname)) echo $login_uname ;?>">
-    <p><?php if(isset( $login_errors["login_uname_err"])) echo  $login_errors["login_uname_err"]; ?></p>
+
+    <small  class="form-text text-danger">
+       <?php if(isset( $login_errors["login_uname_err"])) echo  $login_errors["login_uname_err"]; ?> 
+    </small> 
+    <small  class="form-text text-danger">
+       <?php if(isset( $db_check_username_err)) echo  $db_check_username_err; ?> 
+    </small>
   </div>
   
-   
-   <input type="password" name="login-pwd" placeholder="password" >
-   <?php if(isset( $login_errors["login_pwd_err"])) echo $login_errors["login_pwd_err"]; ?>
-   <button type="submit" name="login-btn">login</button>
+   <div class="form-group">
+   <input type="password"  class= "form-control" name="login-pwd" placeholder="password" >  
+   <small  class="form-text text-danger">
+   <?php if(isset( $login_errors["login_pwd_err"])) echo $login_errors["login_pwd_err"]; ?> 
+    </small> 
+    <small  class="form-text text-danger">
+       <?php if(isset(   $db_check_password_err)) echo   $db_check_password_err; ?> 
+    </small> 
+  
+   </div>
+     <button class="btn btn-success" type="submit" name="login-btn">login</button>
 
 </form>
 
